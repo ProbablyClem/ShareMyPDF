533890074609cec5ef9f3c6032327e726acd1715
Carlos Ceren Selva <carlos.ceren-selva@etu.univ-grenoble-alpes.fr>
2020-10-28 19:11:38 +0100

    Index Changement pour Nathan le bg

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var socket = require('socket.io');
 app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
 
 var server = app.listen(3000, function(){
   console.log("Listen to request on port 3000");
 });
 
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(morgan('short'));
 app.use(express.static('public'));
 
 var io = socket(server);
