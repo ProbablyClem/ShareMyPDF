dadfcec5ac6d046141f74dbdd497a2532a64c9d7
Thomas Duplessis <thomas.duplessis@etu.univ-grenoble-alpes.fr>
2020-11-06 15:40:02 +0100

    modularite render pdf

--- src/public/salonProf.js
+++ src/public/salonProf.js
@@ -1,85 +1,24 @@
-var pageNumber = 1;
-var rendering = false;
-var toRender = 0;
-var max = 1;
-
-var pageIpt = document.getElementById("pageInput");
-
-var doc = 'example.pdf';
-var pdfjsLib = window['pdfjs-dist/build/pdf'];
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js';
-var loading = pdfjsLib.getDocument(doc);
-
-var canvas = document.getElementById('viewer');
-var ctx = canvas.getContext('2d');
+import {
+    pageNumber,
+    drawPage,
+    prevPage,
+    nextPage,
+    goTo
+} from './render.js'
 
 var socket = io.connect("http://localhost:3000");
 
-function draw() {
-    rendering = true;
-    pageIpt.value = pageNumber;
-    loading.promise.then(function(pdf) {
-        pdf.getPage(pageNumber).then(function(page) {
-            max = pdf.numPages;
+var oldPageNumber = 1;
 
-            var scale = 1.5;
-            var vp = page.getViewport({scale: scale});
-
-            canvas.height = vp.height;
-            canvas.width = vp.width;
-
-            var renderCtx = {
-                canvasContext: ctx,
-                viewport: vp
-            }
-
-            var render = page.render(renderCtx);
-            render.promise.then(function() {
-                rendering = false;
-                
-                if (toRender != 0) {
-                    var temp = toRender;
-                    toRender = 0;
-                    drawPage(temp);
-                }
-            })
-        });
-    }, function (reason) {
-        // PDF loading error
-        console.error(reason);
-    });
-}
-
-function drawPage(x) {
-    if (x >= 1 && x <= max) {
-        if (rendering) {
-            console.log("Add " + x + " to render");
-            toRender = x;
-        } else {
-            pageNumber = x;
-            socket.emit("page", pageNumber);
-            draw();
-        }
-    }
-}
-
-function prevPage() {
-    if (pageNumber > 1) {
-        pageNumber--;
-        drawPage(pageNumber);
-    }
-}
-
-function nextPage() {
-    if (pageNumber < max) {
-        pageNumber++;
-        drawPage(pageNumber);
-    }
-}
-
-function goTo() {
-    var x = Number(pageIpt.value);
-    drawPage(x);
-}
+document.getElementById("prev").addEventListener("click", prevPage);
+document.getElementById("next").addEventListener("click", nextPage);
+document.getElementById("go").addEventListener("click", goTo);
 
 drawPage(pageNumber);
+
+setInterval(() => {
+    if (pageNumber != oldPageNumber) {
+        oldPageNumber = pageNumber;
+        socket.emit("page", pageNumber);
+    }
+}, 50);
