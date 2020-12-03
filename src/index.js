5c7d7f1a5d6b3eb98d5e4be12d564a3b7f1233d6
ProbablyClem <45786362+ProbablyClem@users.noreply.github.com>
2020-12-03 21:16:47 +0100

    creations de nouveaux fichier pour le serveur

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var socket = require('socket.io');
 var multer = require('multer');
 var upload = multer({dest:'uploads/'});
-var pageProf = 1;
+const Salon = require('./Salon');
+const routes = require('./routes');
 
 app = express();
 const morgan = require('morgan');
@@ -10,9 +11,8 @@ const bodyparser = require('body-parser');
 ////////////////////////////////////////
 const fileUpload = require('express-fileupload');
 ////////////////////////////////////////
-var pseudo;
-var code;
-var fName;
+
+var salons = Array();
 ////////////////////////////////////////
 
 var server = app.listen(3000, function(){
@@ -49,78 +49,8 @@ io.on('connection', function(socket){
 
 })
 
-//page accueil
-app.get('/', (req, res) => res.sendFile(__dirname + "/public/vues/accueil.html"));
-
-//redirection
-app.get('/joinRoom',(req,res)=>{
-  res.sendFile(__dirname +'/public/vues/joinRoom.html');
-});
-
-//redirection
-app.get('/createRoom',(req,res)=>{
-  res.sendFile(__dirname +'/public/vues/createRoom.html');
-});
-
-//setPseudo
-app.post('/setPseudo',(req,res)=>{
-  console.log("Pseudo :"+req.body.pseudo)
-  pseudo = req.body.pseudo;
-  if(req.body.join==null){
-    res.redirect('/createRoom');
-  }else{
-    res.redirect('/joinRoom');
-  }
-  res.end()
-});
-
-//getCode
-app.post('/getCode',(req,res)=>{
-  console.log("Code :"+req.body.code)
-  const code = req.body.code;
-  res.render("eleve", {salon: code, username: pseudo});
-  res.end()
-});
-
-//getParametre
-app.get('/room/:room',(req,res)=>{
-  code = req.params.room;
-  console.log("Code :"+code);
-  res.sendFile(__dirname + '/public/vues/param.html');
-});
-
-//setPseudo2
-app.post('/param',(req,res)=>{
-  console.log("Pseudo :"+req.body.pseudo);
-  pseudo = req.body.pseudo;
-  res.write("Pseudo : "+pseudo);
-  res.write("\n");
-  res.write("Code : "+code);
-  res.end()
-})
-
-////////////////////////////////////////////////////////////////////////////////
-//Upload
-app.post('/setPDF', upload.single('profile'), (req, res) => {
-  if(!req.files.f){
-    res.send(400);
-  }
-  console.log(req.files.f);
-  fName = req.files.f.name;
-  let avatar = req.files.f;
-  avatar.mv('uploads/' + avatar.name);
-  res.send("Fichier : "+req.files.f.name + "<br> Pseudo : "+pseudo);
-});
-////////////////////////////////////////////////////////////////////////////////
-
-//tests
-app.get("/lecteur", (req,res) =>{
-  res.render("eleve", {salon: 1234, username: "clement"});
-})
-
-app.get("/presentateur", (req,res) =>{
-  res.render("presentateur", {salon: 1234, username: "clement"});
-})
+routes(app);
 
 module.exports = app;
 module.exports = server;
+module.exports = salons;
