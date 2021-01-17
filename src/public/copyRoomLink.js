25ce7954bc6987651be265a5577833549d1407cc
Thomas Duplessis <thomas.duplessis@etu.univ-grenoble-alpes.fr>
2021-01-17 19:40:41 +0100

    room code

--- src/public/copyRoomLink.js
+++ src/public/copyRoomLink.js
@@ -11,4 +11,18 @@ function copyRoomLink(){
     window.alert("URL de la page copiée dans le presse-papier");
 }
 
+function copyRoomCode(){    
+    const el = document.createElement('textarea');
+    el.value = document.getElementById("room").innerHTML;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    window.alert("Code du salon copié dans le presse-papier");
+}
+
 document.getElementById("invite").addEventListener("click", copyRoomLink);
+document.getElementById("copyRoomCode").addEventListener("click", copyRoomCode);
